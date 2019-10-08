d4a7e093ca6b3b5ed8f74f649ed77c7c01db5abb
Josue10599 <josue10599@gmail.com>
2019-10-07 21:52:26 -0300

    Projeto sincronizado com o Firebase, adicionado icone, acao do botao abrir camera e captura de texto.

--- app/src/main/java/com/josuelopes/readtext/firebase/FirebaseLeituraImagem.java
+++ app/src/main/java/com/josuelopes/readtext/firebase/FirebaseLeituraImagem.java
@@ -1,48 +1,25 @@
 package com.josuelopes.readtext.firebase;
 
-import android.content.Context;
 import android.graphics.Bitmap;
-
-import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
-import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
-import com.josuelopes.readtext.R;
 
 public class FirebaseLeituraImagem {
 
-    private final Context context;
-    private FirebaseVisionImage mVisionImage;
-
-    public FirebaseLeituraImagem(Bitmap bitmap, Context context) {
-        mVisionImage = FirebaseVisionImage.fromBitmap(bitmap);
-        this.context = context;
+    public void lerTexto(Bitmap bitmap, OnSuccessListener<FirebaseVisionText> sucesso, OnFailureListener falha) {
+        reconhecerTextoNaImagem(bitmap, sucesso, falha);
     }
 
-    public String lerTexto() {
-        return reconhecerTextoNaImagem();
-    }
-
-    private String reconhecerTextoNaImagem() {
-        final String[] textoExtraido = {""};
-        FirebaseVisionTextRecognizer reconhecerTexto = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
-        reconhecerTexto.processImage(mVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
-            @Override
-            public void onSuccess(FirebaseVisionText firebaseVisionText) {
-                String text = firebaseVisionText.getText();
-                textoExtraido[0] = text;
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                textoExtraido[0] = context.getString(R.string.erro_leitura_imagem);
-            }
-        });
-        return textoExtraido[0];
+    private void reconhecerTextoNaImagem(Bitmap bitmap, OnSuccessListener<FirebaseVisionText> sucesso, OnFailureListener falha) {
+        FirebaseVisionImage visionImage = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVision.getInstance().getOnDeviceTextRecognizer()
+                .processImage(visionImage)
+                .addOnSuccessListener(sucesso)
+                .addOnFailureListener(falha);
     }
 
 }
--- app/src/main/java/com/josuelopes/readtext/ui/FotoActivity.java
+++ app/src/main/java/com/josuelopes/readtext/ui/FotoActivity.java
@@ -14,7 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.josuelopes.readtext.R;
+import com.josuelopes.readtext.firebase.FirebaseLeituraImagem;
 
 import java.util.Objects;
 
@@ -23,11 +26,23 @@ public class FotoActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_CAMERA = 2;
     public static final int REQUEST_CODE_IMAGE_CAMERA = 1;
 
+    private FirebaseLeituraImagem leituraImagem;
+    private TextInputEditText campoTextoExtraido;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.foto_main);
         if (savedInstanceState == null) verificarPermissao();
+        leituraImagem = new FirebaseLeituraImagem();
+        campoTextoExtraido = findViewById(R.id.foto_texto_extraido_textInputEditText);
+        configuraFloatingActionButton();
+    }
+
+    private void configuraFloatingActionButton() {
+        FloatingActionButton floatingActionButton = findViewById(R.id.foto_abrir_camera_floatingActionButton);
+        floatingActionButton.setOnClickListener((view) -> verificarPermissao());
     }
 
     private void abrirCameraDoCelular() {
@@ -38,11 +53,9 @@ public class FotoActivity extends AppCompatActivity {
     private void verificarPermissao() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
-        } else {
-            abrirCameraDoCelular();
-        }
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CODE_CAMERA);
+        } else abrirCameraDoCelular();
     }
 
     @Override
@@ -54,14 +67,20 @@ public class FotoActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        ImageView viewImagem = findViewById(R.id.imagemRetirada);
+        ImageView viewImagem = findViewById(R.id.foto_imagem_retirada_imageView);
         if (requestCode == REQUEST_CODE_IMAGE_CAMERA && resultCode == RESULT_OK) {
             if (data != null) {
                 Bitmap imageBitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
                 viewImagem.setImageBitmap(imageBitmap);
-            } else {
-                viewImagem.setImageDrawable(getDrawable(R.drawable.ic_error_outline_black_24dp));
-            }
-        }
+                leituraImagem.lerTexto(imageBitmap,
+                        firebaseVisionText -> campoTextoExtraido.setText(firebaseVisionText.getText()),
+                        e -> campoTextoExtraido.setText(getString(R.string.erro_leitura_imagem)));
+            } else imagemErro(viewImagem);
+        } else imagemErro(viewImagem);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void imagemErro(ImageView viewImagem) {
+        viewImagem.setImageDrawable(getDrawable(R.drawable.ic_error_outline_black_24dp));
     }
 }
